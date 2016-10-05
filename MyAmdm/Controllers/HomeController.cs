19024154287c695eb7dc1538b500d05343a484b8
Alexey Tereshko <treshaalesha@gmail.com>
2016-10-05 18:38:30 +0300

    Merge8

--- MyAmdm/Controllers/HomeController.cs
+++ MyAmdm/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
-﻿using MyAmdm.Models;
+﻿using HtmlAgilityPack;
+using MyAmdm.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -15,7 +17,7 @@ namespace MyAmdm.Controllers
         {
             IEnumerable<Author> authors = db.Authors;
             ViewBag.Authors = authors;
-
+            ParseInformation();
             return View();
         }
 
@@ -37,6 +39,35 @@ namespace MyAmdm.Controllers
 
             return View();
         }
+
+        public List<string> ParseInformation()
+        {
+            List<string> authorNames = new List<string>();
+            string html = "http://amdm.ru/chords/";
+            HtmlDocument HD = new HtmlDocument();
+            var web = new HtmlWeb
+            {
+                AutoDetectEncoding = false,
+                OverrideEncoding = Encoding.UTF8,
+            };
+            HD = web.Load(html);
+
+            // Собственно, здесь и производится выборка интересующих нодов
+            // В данном случае выбираем блочные элементы с классом eTitle
+            HtmlNodeCollection NoAltElements = HD.DocumentNode.SelectNodes("//td[@class='artist_name']");
+
+            if (NoAltElements != null)
+            {
+                foreach (HtmlNode HN in NoAltElements)
+                {
+                    //Получаем строчки
+                   authorNames.Add(HN.InnerText); //добавление имени в список
+                }
+            }
+            return authorNames;
+        }
+
+        
 
         [HttpGet]
         public ActionResult OpenSongLyric(int id)
